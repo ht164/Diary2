875776185c58d2f3f496300f8e1a4d5fffcaca2c
ht164 <ht164.pulsar@gmail.com>
2014-08-10 15:51:18 +0900

    date and startDate query strings

--- Diary2/mymodules/controller.js
+++ Diary2/mymodules/controller.js
@@ -4,6 +4,7 @@
 
 var DiaryModel = require('../mymodules/diarymodel').model;
 var DiaryFuncs = require('../mymodules/diarymodel').funcs;
+var util = require('../mymodules/util');
 
 function Controller() {
 }
@@ -22,6 +23,45 @@ Controller.prototype = {
     	  res.json(models);
     }
     me.getDiaryModels({}, onGetDiaryModels);
+  },
+
+  /**
+   * create condition object.
+   *
+   * @param query
+   * @return condition object.
+   */
+  createCondition: function(query) {
+    var cond = {};
+
+    if (query.startDate) {
+    	  var startDateStr = query.startDate;
+    	  cond.startDate = new Date(startDateStr.substr(0, 4) + "-" + startDateStr.substr(4, 2) + "-" + startDateStr.substr(6, 2));
+    }
+
+    if (query.date) {
+    	  var dateStr = query.date;
+    	  var startYearStr, startMonthStr, startDateStr;
+    	  var endYearStr, endMonthStr, endDateStr;
+
+    	  // year
+    	  endYearStr = startYearStr = dateStr.substr(0, 4);
+    	  // month
+    	  if (dateStr > 4) {
+    	    endMonthStr = startMonthStr = dateStr.substr(4, 2);
+    	  } else {
+    	    endMonthStr = "12";
+    	  }
+    	  // date
+    	  if (dateStr > 6) {
+    	    endDateStr = startDateStr = dateStr.substr(6, 2);
+    	  } else {
+    	    endDateStr = util.getLastDayOfMonth(endYearStr, endMonthStr);
+    	  }
+
+    	  cond.startDate = cond.startDate || new Date(startYearStr + "-" + startMonthStr + "-" + startDateStr);
+    	  cond.endDate = new Date(endYearStr + "-" + endMonthStr + "-" + endDateStr);
+    }
   },
 
   /**
---
+++ Diary2/mymodules/util.js
@@ -0,0 +1,22 @@
+/**
+ * utility functions.
+ */
+
+var util = {
+  /**
+   * get last day of month.
+   *
+   * @param year year
+   * @param month month
+   * @return the last day of month.
+   */
+  getLastDayOfMonth: function(year, month) {
+    var date = new Date(year + "-" + month + "-01");
+    date.setMonth(date.getMonth() + 1);
+    date.setDate(1);
+    date.setTime(date.getTime() - 1);
+    return date.getDate();
+  }
+}
+
+module.exports = util;
