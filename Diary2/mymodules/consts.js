b9548501f9801e138e6420317c13e3f845aa3b56
ht164 <ht164.pulsar@gmail.com>
2014-08-17 00:22:38 +0900

    define const value.

--- Diary2/mymodules/consts.js
+++ Diary2/mymodules/consts.js
@@ -6,7 +6,8 @@ var consts = {
     siteTitle: "ふにゃふにゃブログ",
     siteSubTitle: "ht164さんの日常",
     siteIntroductionTitle: "このサイトについて",
-    siteIntroduction: "ht164が日記を書き連ねていくだけのサイトです。"
+    siteIntroduction: "ht164が日記を書き連ねていくだけのサイトです。",
+    recentEntryTitle: "最新の日記"
 };
 
 module.exports = consts;
