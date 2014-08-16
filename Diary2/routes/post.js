0ac3cf070a8d5fb3b2eb36db2f077aca85561512
ht164 <ht164.pulsar@gmail.com>
2014-08-16 15:49:04 +0900

    create post request.

---
+++ Diary2/routes/post.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var Controller = require('../mymodules/controller');
+
+/* POST diary entry. */
+router.post('/', function(req, res) {
+});
+
+module.exports = router;
