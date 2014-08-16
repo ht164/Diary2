7da355bf38dbde36e5563d8a8e97a79098d44b57
ht164 <ht164.pulsar@gmail.com>
2014-08-17 02:50:34 +0900

    use log4js as logger. but doesn't work now.

--- Diary2/app.js
+++ Diary2/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var logger = require('./mymodules/logger');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
@@ -17,7 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(logger.loggerHttp);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
