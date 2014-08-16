8f785384ba781d88dfd0c95d0aebe1070255fd6d
ht164 <ht164.pulsar@gmail.com>
2014-08-16 23:43:15 +0900

    show site introduction. change style.

--- Diary2/routes/index.js
+++ Diary2/routes/index.js
@@ -4,7 +4,11 @@ var consts = require('../mymodules/consts');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: consts.siteTitle });
+  res.render('index', {
+    title: consts.siteTitle,
+    siteIntroductionTitle: consts.siteIntroductionTitle,
+    siteIntroduction: consts.siteIntroduction
+  });
 });
 
 module.exports = router;
