4dcd60ed52931bc069c1ddac365afb0d634ddbc1
ht164 <ht164.pulsar@gmail.com>
2014-08-21 23:43:21 +0900

    title on navbar has link to diary site.

--- Diary2/mymodules/consts.js
+++ Diary2/mymodules/consts.js
@@ -5,6 +5,8 @@
 var consts = {
     siteTitle: "ふにゃふにゃブログ",
     siteSubTitle: "ht164さんの日常",
+    siteUrl: "//blog.ht164.jp",
+
     siteIntroductionTitle: "このサイトについて",
     siteIntroduction: "ht164が日記を書き連ねていくだけのサイトです。",
     recentEntryTitle: "最新の日記",
