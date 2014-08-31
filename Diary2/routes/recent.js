2cbe100af8fb0a93cfe5939b2489cde0c08ce6b1
ht164 <ht164.pulsar@gmail.com>
2014-09-01 01:29:24 +0900

    create entry point of getting recent diary list.

---
+++ Diary2/routes/recent.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var Controller = require('../mymodules/controller');
+
+/* get recent diary list */
+router.get('/diary', function(req, res) {
+  // send get request.
+  var controller = new Controller();
+  controller.getRecentDiaries(req, res);
+});
+
+module.exports = router;
