87c4edd783d55c823a27bf1c3d6e75470f3a7fdd
ht164 <ht164.pulsar@gmail.com>
2014-09-07 00:32:11 +0900

    create caption (year/month, prev and next month button).

--- Diary2/public/js/calendar.js
+++ Diary2/public/js/calendar.js
@@ -53,46 +53,75 @@ define(["jquery", "underscore"], function($, _){
         _createDom: function(){
             var me = this;
 
-            var fragment = "<table>";
+            // year/month.
+            var captionElement = 
+            (function(){
+                var table = $("<table></table>");
+                var tr = $("<tr></tr>");
+                table.append(tr);
 
-            // week.
-            fragment += "<tr>";
-            for (var week = 0; week < 7; week++) {
-                fragment += "<td"
-                    + (week == 0 ? " class='week-sun'" : 
-                        (week == 6 ? " class='week-sat'" : ""))
-                    + ">" + me.WEEK_STRING[week] + "</td>";
-            }
-            fragment += "</tr>";
+                var tdPrevBtn = $("<td class='prev-month'></td>");
+                var prevBtn = $("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-chevron-left'></span></button>");
+                tdPrevBtn.append(prevBtn);
+                tr.append(tdPrevBtn);
 
-            // days.
-            // what day is year/month/01 ?
-            var day = (new Date(me.year + "-" + (me.month < 10 ? "0" + me.month : me.month) + "-01")).getDay();
-            var currentDate = 0 - day + 1;
-            // last date of month?
-            var lastDate = me._getLastDate(me.year, me.month);
+                var tdYearMonth = $("<td class='year-month'>" + me.year + "/" + me.month + "</td>");
+                tr.append(tdYearMonth);
 
-            while(currentDate <= lastDate) {
+                var tdNextBtn = $("<td class='next-month'></td>");
+                var nextBtn = $("<button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-chevron-right'></button>");
+                tdNextBtn.append(nextBtn);
+                tr.append(tdNextBtn);
+
+                return table;
+            })();
+
+            // table.
+            var datesElement =
+            (function(){
+                var fragment = "<table class='dates'>";
+
+                // week.
                 fragment += "<tr>";
-                for (var week = 0; week < 7; week++){
+                for (var week = 0; week < 7; week++) {
                     fragment += "<td"
-                    + (week == 0 ? " class='week-sun'" : 
-                        (week == 6 ? " class='week-sat'" : ""))
-                    + ">"
-                    + ((currentDate > 0 && currentDate <= lastDate)
-                        ? (me.datesHavingLink[currentDate]
-                            ? "<a href='" + me.datesHavingLink[currentDate] + "'>" + currentDate + "</a>"
-                            : currentDate )
-                        : "")
-                    + "</td>";
-                    currentDate++;
+                        + (week == 0 ? " class='week-sun'" : 
+                            (week == 6 ? " class='week-sat'" : ""))
+                        + ">" + me.WEEK_STRING[week] + "</td>";
                 }
                 fragment += "</tr>";
-            }
 
-            fragment += "</table>";
+                // days.
+                // what day is year/month/01 ?
+                var day = (new Date(me.year + "-" + (me.month < 10 ? "0" + me.month : me.month) + "-01")).getDay();
+                var currentDate = 0 - day + 1;
+                // last date of month?
+                var lastDate = me._getLastDate(me.year, me.month);
 
-            var dom = $(fragment);
+                while(currentDate <= lastDate) {
+                    fragment += "<tr>";
+                    for (var week = 0; week < 7; week++){
+                        fragment += "<td"
+                        + (week == 0 ? " class='week-sun'" : 
+                            (week == 6 ? " class='week-sat'" : ""))
+                        + ">"
+                        + ((currentDate > 0 && currentDate <= lastDate)
+                            ? (me.datesHavingLink[currentDate]
+                                ? "<a href='" + me.datesHavingLink[currentDate] + "'>" + currentDate + "</a>"
+                                : currentDate )
+                            : "")
+                        + "</td>";
+                        currentDate++;
+                    }
+                    fragment += "</tr>";
+                }
+                fragment += "</table>";
+
+                return $(fragment);
+            })();
+
+            var dom = $("<div></div>");
+            dom.append(captionElement).append(datesElement);
             return dom;
         },
 
