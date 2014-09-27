fa831507bd5a6104569f88c07d0edc47422cc671
ht164 <ht164.pulsar@gmail.com>
2014-09-27 16:01:22 +0900

    create view.monthlydiary module.

---
+++ public/js/view.monthlydiary.js
@@ -0,0 +1,26 @@
+/**
+ * front-end side view.
+ * monthly diary entry.
+ */
+
+define(["util", "jquery", "underscore"], function(Util, $, _){
+    return {
+        /**
+         * show monthly diary entry.
+         */
+        showMonthlyDiaryEntry: function(monthlydiaries){
+            var me = this;
+            if (monthlydiaries.length > 0) {
+                // create html fragment.
+                var fragment = "<ul class='list-unstyled monthly-diary-list'>";
+                _.each(monthlydiaries, function(diary){
+                    var str = "" + diary.year + "-" + diary.month + " (" + diary.count + ")";
+                    var href = Util.generateMonthLinkUrl(diary.year, diary.month);
+                    fragment += "<li><a href='" + href + "'>" + str + "</a></li>";
+                });
+                fragment += "</ul>";
+                $("div#monthlyDiaryBlock").html(fragment);
+            }
+        }
+    };
+});
